8b398b86d09ea266b273eaed4af39c8691cd9103
Thomas Solow <thomassolow@gmail.com>
2015-11-25 17:32:15 -0500

    everything works

--- js/giphy.js
+++ js/giphy.js
@@ -1,35 +1,44 @@
-searchTerm = '';
-var numGifs = 10;
-offset = 0;
-limit = 10;
-key = "dc6zaTOxFJmzC";
-url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key="+key+"&limit="+limit+"&offset="+offset;
-$("body").append($("<img src=http://giphy.com/gifs/robot-valvrave-harakiri-DUG89FKzbL5Bu />"))
+var searchTerm = '';
+var offset = 0;
+var limit = 10;
+var hardLimit = 10;
+var key = "dc6zaTOxFJmzC";
 
 $('form').on('submit', function(e) {
-
   e.preventDefault();
-
   searchTerm = $("#keyword").val();
-  url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key="+key+"&limit="+limit+"&offset="+offset;
+  offset = 0;
+  limit = 10;
+  ajaxCall();
 
+});
+
+
+function ajaxCall() {
+  url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm.replace(/ /g,"+")+"&api_key="+key+"&limit="+limit+"&offset="+offset;
   $.ajax({
       url: url,
       type: "get",
       dataType: "json"
   }).done(function(response){
-      console.log(response.data[0])
-      newImg = $("<img src ='' />");
-      newImg.attr('src', response.data[0].url)
-      $("body").append(newImg)
-
+    for(var i = 0; i<hardLimit; i++){
+      if (response.data[i]){
+        newImg = $("<img src ='' />");
+        newImg.attr('src', response.data[i].images.original.url)
+        $("body").append(newImg)
+      }else {
+        return;
+      }
+    }
+    offset += 10;
+    limit += 10;
   }).fail(function(){
       console.log("Ajax request fails!")
   })
+}
 
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      ajaxCall();
+   }
 });
-// q - search query term or phrase
-// limit - (optional) number of results to return, maximum 100. Default 25.
-// offset - (optional) results offset, defaults to 0.
-// rating - limit results to those rated (y,g, pg, pg-13 or r).
-// fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
