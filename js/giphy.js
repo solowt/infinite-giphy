cefb2e4a8abb91f668c860828e705f6a5ccb5a21
Thomas Solow <thomassolow@gmail.com>
2015-11-25 14:50:22 -0500

    small change

--- js/giphy.js
+++ js/giphy.js
@@ -1,4 +1,6 @@
-var searchTerm = $("#keyword").val();
+$("button").click(function(){
+  system.log($("#keyword").val());
+})
 
 // q - search query term or phrase
 // limit - (optional) number of results to return, maximum 100. Default 25.
@@ -6,10 +8,8 @@ var searchTerm = $("#keyword").val();
 // rating - limit results to those rated (y,g, pg, pg-13 or r).
 // fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
 
-//giphy url, what kind of json will this return? array of gifs or 1 gif only?
-offset= 
+var numGifs = 10;
+offset = 0;
 limit = 10;
 key = "dc6zaTOxFJmzC";
-url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key="+key+"&limit="+limit+"";
-
-var numGifs = 10;
+//url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key="+key+"&limit="+limit+"&offset="+offset;
