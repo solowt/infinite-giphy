1e4390c966efb04f9b4a9c15952ec168ced721da
Thomas Solow <thomassolow@gmail.com>
2015-11-25 20:12:23 -0500

    changed structure around, removed some variables

--- js/giphy.js
+++ js/giphy.js
@@ -1,31 +1,19 @@
-//var for the search term
-var searchTerm = '';
-//var for the offset, aka the index of the array of gifs that we want to start with
-var offset = 0;
-//limit the number of gifs we request
-var limit = 10;
-//variable for the loop that draws the images
-var hardLimit = 10;
 //public key
 var key = "dc6zaTOxFJmzC";
-
+var searchTerm; //I want this to be global
 //on submit listener for the form
 $('form').on('submit', function(e) {
   e.preventDefault();
-  //sets the search term
-  searchTerm = $("#keyword").val();
-  //reset offset and limit when there's a new submission
-  offset = 0;
-  limit = 10;
   //makes ajaxcall
-  ajaxCall();
-
+  ajaxCall(0);
 });
 
-//here's the ajax call
-function ajaxCall() {
+//here's the ajax call, takes the offset which is the index of the image to start with
+function ajaxCall(offset) {
+  //get search term from inside input box
+  searchTerm = $("#keyword").val();
   //construct the URL using variables from above
-  url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm.replace(/ /g,"+")+"&api_key="+key+"&limit="+limit+"&offset="+offset;
+  url = "http://api.giphy.com/v1/gifs/search?q="+searchTerm.replace(/ /g,"+")+"&api_key="+key+"&limit=10&offset="+offset;
   //the call
   $.ajax({
       url: url,
@@ -33,9 +21,9 @@ function ajaxCall() {
       dataType: "json"
   }).done(function(response){
     //draw the images based on the response from the call
-    for(var i = 0; i<hardLimit; i++){
+    for(var i = 0; i<10; i++){
       if (response.data[i]){
-        newImg = $("<img src ='' />");
+        newImg = $("<img class="+searchTerm+" src ='' />"); //draw image, set class to be search term
         newImg.attr('src', response.data[i].images.original.url)
         $("body").append(newImg)
       }else {
@@ -43,9 +31,6 @@ function ajaxCall() {
         return;
       }
     }
-    //increas the offset and limit for the next call
-    offset += 10;
-    limit += 10;
   }).fail(function(){
     //in case of a failure
     console.log("Ajax request fails!")
@@ -57,7 +42,7 @@ $(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
      //make the call again, will have different offset and limit
      if (searchTerm){ //check to see if the search term exists before calling api
-       ajaxCall();
+       ajaxCall($("img."+searchTerm).length); //use the number of images to determine the offset, checks for class which was assigned in ajaxcall method
      }
    }
 });
